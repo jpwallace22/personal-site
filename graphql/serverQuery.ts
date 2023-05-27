ca37561bf46b6693f6f46902a81960bed38495be
jpwallace22 <jpwallace22@gmail.com>
2023-05-27 15:14:25 -0700

    build: configure codegen

--- graphql/serverQuery.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentNode } from "graphql"
-import { getClient } from "graphql/client"
-
-const serverQuery = async (query: DocumentNode) => {
-  const { data } = await getClient().query({ query })
-  return data
-}
-
-export default serverQuery
