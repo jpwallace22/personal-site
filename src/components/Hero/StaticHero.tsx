165484439c69b6acc53f4acf9f193d92c6f6dcf4
jpwallace22 <jpwallace22@gmail.com>
2023-06-11 16:56:14 -0700

    perf(hero): adjust image priority

--- src/components/Hero/StaticHero.tsx
+++ src/components/Hero/StaticHero.tsx
@@ -26,14 +26,13 @@ const StaticHero = () => {
   return (
     <Section>
       <div className="relative h-[80vh]">
-        <Circle dots className="left-1/2" />
+        <Circle dots className="left-1/2" priority />
         <Circle size="lg" contrast="low" className="bottom-1/2 right-1/3 lg:right-1/2" />
         <Circle size="md" contrast="high" className="left-3/4 top-12" />
         <div className={twJoin(styles.wrapper)}>
           <DrawnArrow className="absolute bottom-[115%] right-3/4 hidden w-96 lg:block" animated />
           {logoPath && (
             <Image
-              priority
               src={logoPath}
               width={130}
               height={130}
