5e8dee5d599843733d5d33ecb758eb57ec4f1c06
jpwallace22 <jpwallace22@gmail.com>
2023-06-24 13:23:00 -0700

    feat: useLocalStorage hook

---
+++ src/utils/hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+// TODO create a more robust version of this hook
+
+const useLocalStorage = <T>(key: string, defaultValue: T) => {
+  const [value, setValue] = useState<Maybe<T>>(null)
+
+  // useEffect to set default to prevent RSC issues (hydration mismatch) if used to conditionally render
+  useEffect(() => {
+    let currentValue
+    try {
+      currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue))
+    } catch (e) {
+      currentValue = defaultValue
+    }
+    setValue(currentValue as T)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const setLocalStorage = (newValue: T) => {
+    localStorage.setItem(key, JSON.stringify(newValue))
+    setValue(newValue)
+  }
+
+  return [value, setLocalStorage] as const
+}
+
+export default useLocalStorage
