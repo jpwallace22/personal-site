ac9e2f6e2bfe5cc8f132c0452f81dcdcc810d7da
jpwallace22 <jpwallace22@gmail.com>
2024-02-27 19:03:38 -0800

    feat(Projects): adds tracing beam

--- src/components/ProjectsListing/index.tsx
+++ src/components/ProjectsListing/index.tsx
@@ -3,6 +3,7 @@ import Circle from "@molecules/Circle"
 import Heading, { HeadingMarkup } from "@molecules/Heading"
 import Section from "@molecules/Section"
 import ProjectCard from "@components/ProjectsListing/ProjectCard"
+import { TracingBeam } from "@components/ProjectsListing/TracingBeam"
 
 export type ProjectListingProps = Clean<ProjectListingFragment> & ComponentPropsWithoutRef<"div">
 
@@ -16,19 +17,21 @@ const ProjectListing: FC<ProjectListingProps> = ({
 }) => {
   return (
     <Section wrapperClass={`bg-${bgColor}`} id={sectionId || undefined} className="relative">
-      <Heading as={headingAs as HeadingMarkup} headline={heading} body={body} />
-      <div className="mt-12 flex flex-col items-center gap-8">
-        {cards.map((card, i) => (
-          <ProjectCard key={card.title} reverse={i % 2 === 0} {...card} />
-        ))}
-      </div>
-      {cards.length > 3 && (
-        <Circle
-          size="lg"
-          contrast="low"
-          className="absolute bottom-1/2 right-3/4 hidden md:block"
-        />
-      )}
+      <TracingBeam>
+        <Heading as={headingAs as HeadingMarkup} headline={heading} body={body} />
+        <div className="mt-12 flex flex-col items-center gap-8">
+          {cards.map((card, i) => (
+            <ProjectCard key={card.title} reverse={i % 2 === 0} {...card} />
+          ))}
+        </div>
+        {cards.length > 3 && (
+          <Circle
+            size="lg"
+            contrast="low"
+            className="absolute bottom-1/2 right-3/4 hidden md:block"
+          />
+        )}
+      </TracingBeam>
     </Section>
   )
 }
